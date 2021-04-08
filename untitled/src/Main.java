345923ee6f9a2938810ebcf89e6d6d0122f50f23
tzio shiber <tzion@gmail.com>
2021-04-08 20:31:43 +0300

    Initial commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+       // Scanner in = new Scanner(System.in);
+       // Random r = new Random();
+        int count1=0;
+        int count2=0;
+        int count3=0;
+        for (int i=1;i<=10;i++){
+            count1++;
+            for (int j=1;j<=10;j++){
+                count2++;
+                for(int h=1;h<=10
+                        ;h++){
+                    count3++;
+                }
+            }
+        }
+        System.out.println("count1 = " +count1);
+        System.out.println("count2 = " +count2);
+        System.out.println("count3 = " +count3);
+    } // end main()
+}
+
+
+
